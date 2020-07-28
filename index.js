63379f6d26db14b5aa61ee27515dd4adf0816097
William Brown <wbro0105@gmail.com>
2020-07-28 16:41:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var a = "william brown";
+var b = "4444 seasame.st"
+var c = "science   math   english   history"
+var d = " 98       79        87      74"
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
